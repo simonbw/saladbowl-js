edecabcf47db27b259930293544df5dca1abb489
simon <simonbw@uw.edu>
2015-12-31 18:11:31 -0800

    Refresh

--- web/refresh.js
+++ web/refresh.js
@@ -2,23 +2,48 @@ var Game = require('../shared/Game');
 
 var REFRESH_DELAY = 1500;
 
-module.exports = function (template, handler, data) {
+var Refresh = {};
+
+module.exports = Refresh;
+
+var oldDatas = {};
+
+/**
+ * Setup auto refreshing.
+ *
+ * @param template
+ * @param handler
+ * @param data
+ */
+Refresh.auto = function (template, handler, data, delay) {
+  delay = delay || REFRESH_DELAY;
   if (handler) {
     handler(data);
   }
-  setInterval(function () {
-    $.get('').done(function (data) {
-      data = processResponse(data);
-      $('#main-content').html(Handlebars.templates[template](data));
-      if (handler) {
-        handler(data);
-      }
-    }).fail(function (error) {
-      console.log('ERROR:', error);
-    });
-  }, REFRESH_DELAY);
+  setInterval(Refresh.refresh.bind(this, template, handler), delay);
 };
 
+/**
+ * Refresh a page.
+ *
+ * @param template
+ * @param handler
+ */
+Refresh.refresh = function (template, handler) {
+  $.get('').done(function (data) {
+    data = processResponse(data);
+    var oldData = oldDatas[template];
+    oldDatas[template] = data;
+    if (JSON.stringify(oldData) != JSON.stringify(data)) { // only refresh on new data
+      $('#main-content').html(Handlebars.templates[template](data));
+    }
+    if (handler) {
+      handler(data);
+    }
+  }).fail(function (error) {
+    console.log('ERROR:', error);
+  });
+};
 
 /**
  * Process all the bits of the response.
