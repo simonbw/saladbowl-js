fa090d0e25dd4ccc9246c56ffe611d6fa471eacb
Simon <simonbw@uw.edu>
2018-08-15 10:15:42 -0700

    Fix nosleep bug

--- js/web/gamePage.js
+++ js/web/gamePage.js
@@ -1,19 +1,18 @@
-'use strict';
+"use strict";
 
-window.Promise = window.Promise || require('promise-polyfill');
-require('setimmediate');
+window.Promise = window.Promise || require("promise-polyfill");
+require("setimmediate");
 
-const Immutable = require('immutable');
-const io = require('socket.io-client');
-const ReduxProvider = require('react-redux').Provider;
-const React = require('react');
-const ReactDom = require('react-dom');
-const NoSleep = require('./NoSleep');
+const Immutable = require("immutable");
+const io = require("socket.io-client");
+const ReduxProvider = require("react-redux").Provider;
+const React = require("react");
+const ReactDom = require("react-dom");
+const disableSleeping = require("./NoSleep");
 
-const ActionTypes = require('../shared/ActionTypes.js');
-const Store = require('./Store');
-const GameApp = require('./components/GameApp');
-const SocketHandlers = require('./socket/SocketHandlers');
+const Store = require("./Store");
+const GameApp = require("./components/GameApp");
+const SocketHandlers = require("./socket/SocketHandlers");
 
 /**
  * Render the page and open the socket.
@@ -21,13 +20,15 @@ const SocketHandlers = require('./socket/SocketHandlers');
 window.onload = () => {
   const initialState = Immutable.fromJS({
     ui: {
-      userName: localStorage.getItem('userName')
+      userName: localStorage.getItem("userName")
     },
     userId: window.INITIAL_USER_ID,
     game: window.INITIAL_GAME
   });
 
-  const socket = io('/', {query: 'gameId=' + initialState.get('game').get('id')});
+  const socket = io("/", {
+    query: "gameId=" + initialState.get("game").get("id")
+  });
   SocketHandlers.init(socket);
   const store = Store(initialState, socket, window.DEBUG_MODE);
   socket.store = store; // TODO: Don't do this
@@ -39,15 +40,15 @@ window.onload = () => {
     window.DEBUG = {
       socket: socket,
       store: store,
-      GameHelpers: require('../shared/GameHelpers')
+      GameHelpers: require("../shared/GameHelpers")
     };
   }
 
-  const enableNoSleep = () => {
-    (new NoSleep()).enable();
-    document.addEventListener('touchstart', enableNoSleep, false);
+  const onTouchStart = () => {
+    disableSleeping();
+    document.removeEventListener("touchstart", onTouchStart);
   };
-  document.addEventListener('touchstart', enableNoSleep, false);
+  document.addEventListener("touchstart", onTouchStart);
 };
 
 /**
@@ -58,6 +59,6 @@ function render(store) {
     <ReduxProvider store={store}>
       <GameApp />
     </ReduxProvider>,
-    document.getElementById('game-react')
+    document.getElementById("game-react")
   );
 }
